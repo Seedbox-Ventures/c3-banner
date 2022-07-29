cfce975cd3e5af0103f086fd7313b8bfcb4f8d4d
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2022-07-29 11:39:17 +0200

    task: Remove activation codes from cookie definitions and establish presets structure

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -16,35 +16,6 @@ export default {
     cookies: "_fbp,act,c_user,datr,fr,m_pixel_ration,pl,presence,sb,spin,wd,xs",
     period: "Session / 1 Jahr",
     info: "Cookie von Meta, das für Website-Analysen, Ad-Targeting und Anzeigenmessung verwendet wird.",
-    activationCode: (metaPixelId) => {
-      if (window.doNotTrack !== 1) {
-        !(function (f, b, e, v, n, t, s) {
-          if (f.fbq) return;
-          n = f.fbq = function () {
-            n.callMethod
-              ? n.callMethod.apply(n, arguments)
-              : n.queue.push(arguments);
-          };
-          if (!f._fbq) f._fbq = n;
-          n.push = n;
-          n.loaded = !0;
-          n.version = "2.0";
-          n.queue = [];
-          t = b.createElement(e);
-          t.async = !0;
-          t.src = v;
-          s = b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t, s);
-        })(
-          window,
-          document,
-          "script",
-          "https://connect.facebook.net/en_US/fbevents.js"
-        );
-        fbq("init", metaPixelId);
-        fbq("track", "PageView");
-      }
-    },
     type: SECTION_TYPE_MARKETING,
   },
   ga: {
@@ -52,29 +23,6 @@ export default {
     cookies: "_ga,_gat,_gid",
     period: "2 Jahre",
     info: "Cookie von Google für Website-Analysen. Erzeugt statistische Daten darüber, wie der Besucher die Website nutzt.",
-    activationCode: (ga4MessId) => {
-      if (window.doNotTrack !== 1) {
-        const js = document.createElement("script");
-
-        js.type = "text/javascript";
-        js.async = true;
-        js.src = "https://www.googletagmanager.com/gtag/js?id=G-E35R1TDQ1C";
-
-        js.onload = function () {
-          //Code using this script here
-        };
-
-        document.body.appendChild(js);
-
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-
-        gtag("config", ga4MessId);
-      }
-    },
     type: SECTION_TYPE_ANALYTICS,
   },
   gtm: {
@@ -82,25 +30,6 @@ export default {
     cookies: "_ga,_gat,_gid",
     period: "2 Jahre",
     info: "Cookie von Google zur Steuerung der erweiterten Script- und Ereignisbehandlung.",
-    activationCode: (gtmID) => {
-      if (window.doNotTrack !== 1) {
-        // Define dataLayer and the gtag function.
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        (function (w, d, s, l, i) {
-          w[l] = w[l] || [];
-          w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-          var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != "dataLayer" ? "&l=" + l : "";
-          j.async = true;
-          j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-          f.parentNode.insertBefore(j, f);
-        })(window, document, "script", "dataLayer", gtmID);
-      }
-    },
     type: SECTION_TYPE_ANALYTICS,
   },
   hotjar: {
@@ -109,23 +38,6 @@ export default {
       "_hjClosedSurveyInvites, _hjDonePolls, _hjMinimizedPolls, _hjDoneTestersWidgets, _hjIncludedInSample, _hjShownFeedbackMessage, _hjid, _hjRecordingLastActivity, hjTLDTest, _hjUserAttributesHash, _hjCachedUserAttributes, _hjLocalStorageTest, _hjptid",
     period: "Session / 1 Jahr",
     info: "Hotjar ist ein Analysewerkzeug für das Benutzerverhalten von Hotjar Ltd. Wir verwenden Hotjar, um zu verstehen, wie Benutzer mit unserer Website interagieren.",
-    activationCode: (hjSiteId) => {
-      if (window.doNotTrack !== 1) {
-        (function (h, o, t, j, a, r) {
-          h.hj =
-            h.hj ||
-            function () {
-              (h.hj.q = h.hj.q || []).push(arguments);
-            };
-          h._hjSettings = { hjid: hjSiteId, hjsv: 6 };
-          a = o.getElementsByTagName("head")[0];
-          r = o.createElement("script");
-          r.async = 1;
-          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-      }
-    },
     type: SECTION_TYPE_ANALYTICS,
   },
   linkedin: {
@@ -133,29 +45,6 @@ export default {
     cookies: "li_gc",
     period: "Session / 1 Jahr",
     info: "Cookie von LinkedIn, das für Website-Analysen, Ad-Targeting und Anzeigenmessung verwendet wird.",
-    activationCode: (insightTagId) => {
-      if (window.doNotTrack !== 1) {
-        _linkedin_partner_id = "${insightTagId}";
-        window._linkedin_data_partner_ids =
-          window._linkedin_data_partner_ids || [];
-        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-
-        (function (l) {
-          if (!l) {
-            window.lintrk = function (a, b) {
-              window.lintrk.q.push([a, b]);
-            };
-            window.lintrk.q = [];
-          }
-          var s = document.getElementsByTagName("script")[0];
-          var b = document.createElement("script");
-          b.type = "text/javascript";
-          b.async = true;
-          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-          s.parentNode.insertBefore(b, s);
-        })(window.lintrk);
-      }
-    },
     type: SECTION_TYPE_MARKETING,
   },
   ma: {
@@ -163,29 +52,6 @@ export default {
     cookies: "MUID",
     period: "1 Jahr",
     info: "Cookies von Microsoft Advertising um den Erfolg von Werbemaßnahmen nachzuvollziehen.",
-    activationCode: (uetTag) => {
-      if (window.doNotTrack !== 1) {
-        (function (w, d, t, r, u) {
-          var f, n, i;
-          (w[u] = w[u] || []),
-            (f = function () {
-              var o = { ti: uetTag };
-              (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
-            }),
-            (n = d.createElement(t)),
-            (n.src = r),
-            (n.async = 1),
-            (n.onload = n.onreadystatechange =
-              function () {
-                var s = this.readyState;
-                (s && s !== "loaded" && s !== "complete") ||
-                  (f(), (n.onload = n.onreadystatechange = null));
-              }),
-            (i = d.getElementsByTagName(t)[0]),
-            i.parentNode.insertBefore(n, i);
-        })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
-      }
-    },
     type: SECTION_TYPE_MARKETING,
   },
   stripe: {
@@ -203,59 +69,6 @@ export default {
       "_abck, _ga, _ga_HV1FL86553, _ttp, cmpl_token, cookie-consent, from_way, msToken, odin_tt, passport_auth_status, passport_auth_status_ss, passport_auth_status_ss_ads, passport_csrf_token, passport_csrf_token_default, s_v_web_id, sessionid, sessionid_ads, sessionid_ss, sessionid_ss_ads, sid_guard, sid_guard_ads, sid_tt, sid_tt_ads, sid_ucp_v1, ssid_ucp_sso_v1_ads, ssid_ucp_v1, ssid_ucp_v1_ads, sso_auth_status_ss_ads, sso_uid_tt_ss_ads, store-country-code, store-idc, toutiao_sso_user_ss_ads, tt-target-idc, tt_csrf_token, tt_webid, tta_attr_ga_cid, tta_attr_id, tta_attr_id_mirror, ttwid, uid_tt, uid_tt_ads, uid_tt_ss, uid_tt_ss_",
     period: "Session / 2 Jahre",
     info: "TikTok ist ein Social Media Portal für das Streaming kurzers Videoinhalte.",
-    activationCode: (tiktokPixel) => {
-      if (window.doNotTrack !== 1) {
-        !(function (w, d, t) {
-          w.TiktokAnalyticsObject = t;
-          var ttq = (w[t] = w[t] || []);
-          (ttq.methods = [
-            "page",
-            "track",
-            "identify",
-            "instances",
-            "debug",
-            "on",
-            "off",
-            "once",
-            "ready",
-            "alias",
-            "group",
-            "enableCookie",
-            "disableCookie",
-          ]),
-            (ttq.setAndDefer = function (t, e) {
-              t[e] = function () {
-                t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-              };
-            });
-          for (var i = 0; i < ttq.methods.length; i++)
-            ttq.setAndDefer(ttq, ttq.methods[i]);
-          (ttq.instance = function (t) {
-            for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)
-              ttq.setAndDefer(e, ttq.methods[n]);
-            return e;
-          }),
-            (ttq.load = function (e, n) {
-              var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
-              (ttq._i = ttq._i || {}),
-                (ttq._i[e] = []),
-                (ttq._i[e]._u = i),
-                (ttq._t = ttq._t || {}),
-                (ttq._t[e] = +new Date()),
-                (ttq._o = ttq._o || {}),
-                (ttq._o[e] = n || {});
-              var o = document.createElement("script");
-              (o.type = "text/javascript"),
-                (o.async = !0),
-                (o.src = i + "?sdkid=" + e + "&lib=" + t);
-              var a = document.getElementsByTagName("script")[0];
-              a.parentNode.insertBefore(o, a);
-            });
-          ttq.load(tiktokPixel);
-          ttq.page();
-        })(window, document, "ttq");
-      }
-    },
     type: SECTION_TYPE_MARKETING,
   },
 };
---
+++ src/js/presets.js
@@ -0,0 +1,238 @@
+export const defaultPreset = {
+  ga: {
+    activationCode: function (ga4MessId) {
+      if (window.doNotTrack !== 1) {
+        const js = document.createElement("script");
+
+        js.type = "text/javascript";
+        js.async = true;
+        js.src = "https://www.googletagmanager.com/gtag/js?id=G-E35R1TDQ1C";
+
+        js.onload = function () {
+          //Code using this script here
+        };
+
+        document.body.appendChild(js);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", ga4MessId);
+      }
+    },
+  },
+  gtm: {
+    activationCode: function (gtmID) {
+      if (window.doNotTrack !== 1) {
+        // Define dataLayer and the gtag function.
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        (function (w, d, s, l, i) {
+          w[l] = w[l] || [];
+          w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+          var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : "";
+          j.async = true;
+          j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, "script", "dataLayer", gtmID);
+      }
+    },
+  },
+  hotjar: {
+    activationCode: function (hjSiteId) {
+      if (window.doNotTrack !== 1) {
+        (function (h, o, t, j, a, r) {
+          h.hj =
+            h.hj ||
+            function () {
+              (h.hj.q = h.hj.q || []).push(arguments);
+            };
+          h._hjSettings = { hjid: hjSiteId, hjsv: 6 };
+          a = o.getElementsByTagName("head")[0];
+          r = o.createElement("script");
+          r.async = 1;
+          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+      }
+    },
+  },
+  linkedin: {
+    activationCode: (insightTagId) => {
+      if (window.doNotTrack !== 1) {
+        _linkedin_partner_id = "${insightTagId}";
+        window._linkedin_data_partner_ids =
+          window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+        (function (l) {
+          if (!l) {
+            window.lintrk = function (a, b) {
+              window.lintrk.q.push([a, b]);
+            };
+            window.lintrk.q = [];
+          }
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";
+          b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);
+        })(window.lintrk);
+      }
+    },
+  },
+  ma: {
+    activationCode: function (uetTag) {
+      if (window.doNotTrack !== 1) {
+        (function (w, d, t, r, u) {
+          var f, n, i;
+          (w[u] = w[u] || []),
+            (f = function () {
+              var o = { ti: uetTag };
+              (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+            }),
+            (n = d.createElement(t)),
+            (n.src = r),
+            (n.async = 1),
+            (n.onload = n.onreadystatechange =
+              function () {
+                var s = this.readyState;
+                (s && s !== "loaded" && s !== "complete") ||
+                  (f(), (n.onload = n.onreadystatechange = null));
+              }),
+            (i = d.getElementsByTagName(t)[0]),
+            i.parentNode.insertBefore(n, i);
+        })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+      }
+    },
+  },
+  meta: {
+    activationCode: function (metaPixelId) {
+      if (window.doNotTrack !== 1) {
+        !(function (f, b, e, v, n, t, s) {
+          if (f.fbq) return;
+          n = f.fbq = function () {
+            n.callMethod
+              ? n.callMethod.apply(n, arguments)
+              : n.queue.push(arguments);
+          };
+          if (!f._fbq) f._fbq = n;
+          n.push = n;
+          n.loaded = !0;
+          n.version = "2.0";
+          n.queue = [];
+          t = b.createElement(e);
+          t.async = !0;
+          t.src = v;
+          s = b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t, s);
+        })(
+          window,
+          document,
+          "script",
+          "https://connect.facebook.net/en_US/fbevents.js"
+        );
+        fbq("init", metaPixelId);
+        fbq("track", "PageView");
+      }
+    },
+  },
+  tiktok: {
+    activationCode: function (tiktokPixel) {
+      if (window.doNotTrack !== 1) {
+        !(function (w, d, t) {
+          w.TiktokAnalyticsObject = t;
+          var ttq = (w[t] = w[t] || []);
+          (ttq.methods = [
+            "page",
+            "track",
+            "identify",
+            "instances",
+            "debug",
+            "on",
+            "off",
+            "once",
+            "ready",
+            "alias",
+            "group",
+            "enableCookie",
+            "disableCookie",
+          ]),
+            (ttq.setAndDefer = function (t, e) {
+              t[e] = function () {
+                t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+              };
+            });
+          for (var i = 0; i < ttq.methods.length; i++)
+            ttq.setAndDefer(ttq, ttq.methods[i]);
+          (ttq.instance = function (t) {
+            for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)
+              ttq.setAndDefer(e, ttq.methods[n]);
+            return e;
+          }),
+            (ttq.load = function (e, n) {
+              var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+              (ttq._i = ttq._i || {}),
+                (ttq._i[e] = []),
+                (ttq._i[e]._u = i),
+                (ttq._t = ttq._t || {}),
+                (ttq._t[e] = +new Date()),
+                (ttq._o = ttq._o || {}),
+                (ttq._o[e] = n || {});
+              var o = document.createElement("script");
+              (o.type = "text/javascript"),
+                (o.async = !0),
+                (o.src = i + "?sdkid=" + e + "&lib=" + t);
+              var a = document.getElementsByTagName("script")[0];
+              a.parentNode.insertBefore(o, a);
+            });
+          ttq.load(tiktokPixel);
+          ttq.page();
+        })(window, document, "ttq");
+      }
+    },
+  },
+};
+
+// ssp stands for seedbox super preset
+export const ssp = {
+  hotjar: {
+    activationCode: function (hjSiteId) {
+      if (window.doNotTrack !== 1) {
+        (function (h, o, t, j, a, r) {
+          h.hj =
+            h.hj ||
+            function () {
+              (h.hj.q = h.hj.q || []).push(arguments);
+            };
+          h._hjSettings = { hjid: hjSiteId, hjsv: 6 };
+          a = o.getElementsByTagName("head")[0];
+          r = o.createElement("script");
+          r.async = 1;
+          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const keyword = urlParams.get("kwd");
+
+        if (typeof keyword === "string" && keyword.length) {
+          hj("event", keyword);
+        }
+      }
+    },
+  },
+};
+
+export default {
+  default: defaultPreset,
+  ssp,
+};
